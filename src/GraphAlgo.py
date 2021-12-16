68e2867b84c56927118b6e54be772e4923faf430
Tal Malchi <malchital@gmail.com>
2021-12-16 16:56:13 +0200

    Initial commit

---
+++ src/GraphAlgo.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from GraphAlgoInterface import GraphAlgoInterface #abstractmethod
+from src import GraphInterface
+
+
+class GraphAlgo(GraphAlgoInterface):
+
+    def load_from_json(self, file_name: str) -> bool:
+        pass
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        pass
+
+    def plot_graph(self) -> None:
+        pass
+
+    def get_graph(self) -> GraphInterface:
+        super().get_graph()
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        super().TSP(node_lst)
+
+    def centerPoint(self) -> (int, float):
+        super().centerPoint()
