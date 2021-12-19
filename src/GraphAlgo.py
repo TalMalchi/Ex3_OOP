3f93359b808a37ad6342668d4611f2be17ba26ab
Daniel Appel <appel.daniel1998@gmail.com>
2021-12-19 23:44:09 +0200

    Changes by Tal

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -28,7 +28,40 @@ class GraphAlgo(GraphAlgoInterface):
                 self.g.add_edge(edge["src"], edge["dest"], edge["w"])
 
     def save_to_json(self, file_name: str) -> bool:
-        pass
+        if self.g is None:
+            return False
+        edges = []
+        for n in self.g.get_all_v().keys():
+            for dest in self.g.all_out_edges_of_node(n):
+                edges.append({"src": n, "dest": dest, "w": self.g.all_out_edges_of_node(n).get(dest)})
+
+        nodes = []
+        for key in self.g.get_all_v().keys():
+            nodes.append({"id": key, "pos": self.g.get_all_v()[key]})
+
+        saved_graph = {"Nodes": nodes, "Edges": edges}
+        with open(file_name, 'w') as json_file:
+            json.dump(saved_graph, json_file)
+
+    # def dijkstras_algo(self, src_node: int) -> dict:
+    #     map = {}
+    #     visited = {}
+    #     unvisited = {}
+    #     map.update({src_node: [0, 0.5]})
+    #     unvisited.update({src_node: None})
+    #     for node in self.g.get_all_v().keys():
+    #         if node != src_node:
+    #             map.update({node: [sys.maxsize, 0.5]})
+    #             unvisited.update({node, None})
+    #
+    #     curr_node = src_node
+    #     curr_val = 0
+    #     while not len(unvisited) == 0:
+    #         neighbour_node_edges = self.g.all_out_edges_of_node(curr_node)
+    #         for edge_dest in neighbour_node_edges.keys():
+    #             if neighbour_node_edges[edge_dest] not in
+
+
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         pass
--- src/NodeData.py
+++ src/NodeData.py
@@ -4,3 +4,7 @@ class NodeData:
     def __init__(self, pos, id):
         self.pos = pos
         self.id =id
+
+    def getKey(self) -> int:
+        return self.id
+
